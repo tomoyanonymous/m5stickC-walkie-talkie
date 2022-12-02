de55ff52b1585adede136bc8e9a57757c97f001c
febalci <febalci@yahoo.com>
2022-12-02 16:20:07 +0300

    M5Atom

--- src/config.h
+++ src/config.h
@@ -10,39 +10,35 @@
 #define SAMPLE_RATE 16000
 
 // are you using an I2S microphone - comment this if you want to use an analog mic and ADC input
-// #define USE_I2S_MIC_INPUT
+#define USE_I2S_MIC_INPUT
 
 // I2S Microphone Settings
 
 // Which channel is the I2S microphone on? I2S_CHANNEL_FMT_ONLY_LEFT or I2S_CHANNEL_FMT_ONLY_RIGHT
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-// #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_19
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+// #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
+#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_22 //SCK - BCLK
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_21 //WS - LRC
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_19 //SD
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
 
 // speaker settings
 #define USE_I2S_SPEAKER_OUTPUT
-#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_19
-#define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_5
+#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_22 //BCLK
+#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_21 //LRC
+#define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_25 //DIN
 // Shutdown line if you have this wired up or -1 if you don't
-#define I2S_SPEAKER_SD_PIN GPIO_NUM_22
+#define I2S_SPEAKER_SD_PIN -1 //SD
 
 // transmit button
-#define GPIO_TRANSMIT_BUTTON 23
-
-// Which LED pin do you want to use? TinyPico LED or the builtin LED of a generic ESP32 board?
-// Comment out this line to use the builtin LED of a generic ESP32 board
-// #define USE_LED_GENERIC
+#define GPIO_TRANSMIT_BUTTON 39
 
 // Which transport do you want to use? ESP_NOW or UDP?
 // comment out this line to use UDP
-// #define USE_ESP_NOW
+#define USE_ESP_NOW
 
 // On which wifi channel (1-11) should ESP-Now transmit? The default ESP-Now channel on ESP32 is channel 1
 #define ESP_NOW_WIFI_CHANNEL 1
