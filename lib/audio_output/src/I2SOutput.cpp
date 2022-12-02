51a2d398efa1376384e3008fdee43bdc34459219
febalci <febalci@yahoo.com>
2022-12-02 16:37:37 +0300

    M5Atom

--- lib/audio_output/src/I2SOutput.cpp
+++ lib/audio_output/src/I2SOutput.cpp
@@ -16,14 +16,10 @@ void I2SOutput::start(uint32_t sample_rate)
         .sample_rate = (int)sample_rate, 
 #endif
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 2, 0)
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S),
-#else
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S),
-#endif
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 2,
+        .dma_buf_count = 8, //2,
         .dma_buf_len = 1024,
         .use_apll = false,
         .tx_desc_auto_clear = true,
